9213ca7ae75db553111bd9ead5d81286fe87bd1a
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-26 18:36:29 -0400

    Successful ajax request

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var searchCity;
 $("#searchBtn").on("click", function(event){
     event.preventDefault();
     searchCity = $("#searchInput").val();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=3dc2fb0f58e00d68a8cdc408568026d4"; 
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIkey; 
+    console.log(searchCity);
     $.ajax({
         url: queryURL,
         method: "GET"
